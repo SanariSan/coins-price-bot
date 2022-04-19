e69e24cf1f97afdf75351487f987c3eab6e3b90d
SanariSan <SanariSan@users.noreply.github.com>
2022-04-19 18:13:08 +0300

    feat: per chat limitations

--- bot/bot.js
+++ bot/bot.js
@@ -1,6 +1,6 @@
 import localtunnel from 'localtunnel';
 import { getGmtRate, getGstRate, getSolRate } from '../requests';
-import { log } from '../util';
+import { dir, log } from '../util';
 
 async function setupBotWebhook(bot, webhookSecretPath) {
   const { NODE_ENV, APP_NAME, PORT } = process.env;
@@ -16,32 +16,90 @@ async function setupBotWebhook(bot, webhookSecretPath) {
   bot.telegram.setWebhook(webhookURL);
 }
 
+function getInitChatTimestamps(timeout) {
+  const initTime = Date.now() - timeout;
+  const ts = {
+    gst: initTime,
+    gmt: initTime,
+    sol: initTime,
+  };
+
+  return ts;
+}
+
 function setupBotActions(bot) {
+  const { TIMEOUT_MINS } = process.env;
+  const timeout = 1000 * 60 * TIMEOUT_MINS;
+  const chats = {};
+
   bot.start((ctx) => {
-    ctx.reply('Run command to get current coin price');
+    ctx.reply('Add me to chat and run command to get current coin prices');
   });
-  bot.command('gst', async (ctx) => {
-    const res = await getGstRate().catch((e) => log(e));
-    if (!res) ctx.reply('Try again later');
-    else {
-      log(res);
-      ctx.reply(res);
+
+  // restrict price request in pm with bot
+  bot.use((ctx, next) => {
+    const { SELF_CHAT_ID } = process.env;
+
+    if (ctx.chat.id == SELF_CHAT_ID) return;
+    return next();
+  });
+
+  // initialize own timestamps for every chat
+  bot.use((ctx, next) => {
+    if (!(ctx.chat.id in chats)) {
+      chats[ctx.chat.id] = getInitChatTimestamps(timeout);
+      log(ctx.chat.id);
     }
+    return next();
   });
-  bot.command('gmt', async (ctx) => {
-    const res = await getGmtRate().catch((e) => log(e));
-    if (!res) ctx.reply('Try again later');
-    else {
-      log(res);
-      ctx.reply(res);
-    }
+
+  bot.command('gst', async (ctx, next) => {
+    await ctx.tg.deleteMessage(ctx.chat.id, ctx.message.message_id);
+
+    const nowLocal = Date.now();
+    if (nowLocal - timeout < chats[ctx.chat.id].gst) return;
+    chats[ctx.chat.id].gst = nowLocal;
+
+    ctx.res = getGstRate().catch((e) => log(e));
+    // ctx.res = Promise.resolve().then(() => 'Current gst price: 4.49 usd');
+
+    return next();
   });
-  bot.command('sol', async (ctx) => {
-    const res = await getSolRate().catch((e) => log(e));
-    if (!res) ctx.reply('Try again later');
-    else {
-      log(res);
-      ctx.reply(res);
+
+  bot.command('gmt', async (ctx, next) => {
+    await ctx.tg.deleteMessage(ctx.chat.id, ctx.message.message_id);
+
+    const nowLocal = Date.now();
+    if (nowLocal - timeout < chats[ctx.chat.id].gmt) return;
+    chats[ctx.chat.id].gmt = nowLocal;
+
+    ctx.res = getGmtRate().catch((e) => log(e));
+    // ctx.res = Promise.resolve().then(() => 'Current gmt price: 3.69 usd');
+
+    return next();
+  });
+
+  bot.command('sol', async (ctx, next) => {
+    await ctx.tg.deleteMessage(ctx.chat.id, ctx.message.message_id);
+
+    const nowLocal = Date.now();
+    if (nowLocal - timeout < chats[ctx.chat.id].sol) return;
+    chats[ctx.chat.id].sol = nowLocal;
+
+    ctx.res = getSolRate().catch((e) => log(e));
+    // ctx.res = Promise.resolve().then(() => 'Current sol price: 106.44 usd');
+
+    return next();
+  });
+
+  // general response processor
+  bot.use(async (ctx) => {
+    const res = await ctx.res;
+
+    if (res) {
+      ctx.reply(res).catch((e) => log(e));
+    } else {
+      ctx.reply('Try again later').catch((e) => log(e));
     }
   });
 }
